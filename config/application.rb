f6cde2445e6fd250b8f1f8744b03929e7b942914
subroh0508 <subroh.0508@gmail.com>
2024-11-09 18:50:30 +0900

    Set application config

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module A
+module TrackTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
@@ -38,5 +38,9 @@ module A
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.lookbook.preview_display_options = {
+      theme: %w[light dark],
+    }
   end
 end
