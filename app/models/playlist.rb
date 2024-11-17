4c735fded1f962afa292cd7fad4d99f5c1d071f9
subroh0508 <subroh.0508@gmail.com>
2024-11-17 18:39:08 +0900

    Add build function

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,33 @@ class Playlist < ApplicationRecord
 
   has_many :translations, class_name: 'Translations::Playlist'
   has_many :tracks, through: :playlist_tracks
+
+  def build(params)
+    playlist = Playlist.new(
+      type: type_to_enum(params[:type]),
+      youtube_id: params[:youtube_id],
+    )
+
+    playlist.translations.build(
+      title: params[:title],
+      locale: params[:locale],
+    )
+
+    playlist
+  end
+
+  private
+
+  def type_to_enum(type)
+    case type
+    when 'album'
+      0
+    when 'official'
+      1
+    when 'user'
+      2
+    else
+      throw ArgumentError
+    end
+  end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,16 @@
 class Track < ApplicationRecord
   has_many :translations, class_name: 'Translations::Track'
+
+  def build(params)
+    track = Track.new(
+      youtube_id: params[:youtube_id],
+    )
+
+    track.translations.build(
+      title: params[:title],
+      locale: params[:locale],
+    )
+
+    track
+  end
 end
