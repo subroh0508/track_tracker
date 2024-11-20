1b941e1e48cd2c985a53a6c252df2abaec1dd1c2
subroh0508 <subroh.0508@gmail.com>
2024-11-20 23:39:47 +0900

    Add config to application.(css|js)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "@fortawesome/fontawesome-free"
+
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { library } from "@fortawesome/fontawesome-svg-core"
+
 import "controllers"
+
+library.add(fas, far, fab)
