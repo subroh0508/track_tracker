cfc19ef004c97044fac65522b2f8645d7295ab96
subroh0508 <subroh.0508@gmail.com>
2025-01-03 14:13:22 +0900

    Refactor artist data structure to support multiple artists

--- app/frontend/sections/brands/grid_preview.rb
+++ app/frontend/sections/brands/grid_preview.rb
@@ -9,31 +9,56 @@ class Brands::GridPreview < ViewComponent::Preview
           thumbnail_url: "https://i.scdn.co/image/ab67616d00001e027de4dbffeda7b3ab5a2e5c77",
           title: "Today Is A Beautiful Day",
           year: 2011,
-          artist: "supercell",
+          artists: [
+            {
+              spotify_id: "aaa",
+              name: "supercell",
+            },
+          ],
         },
         {
           thumbnail_url: "https://i.scdn.co/image/ab67616d00001e02bfb129acb345001444dc06cf",
           title: "THE IDOLM@STER ORCHESTRA CONCERT　～SYMPHONY OF FIVE STARS!!!!!～ コンサートアルバム",
           year: 2024,
-          artist: "東京フィルハーモニー交響楽団,THE IDOLM@STER Series,Bandai Namco Game Music",
+          artists: [
+            {
+              spotify_id: "bbb",
+              name: "東京フィルハーモニー交響楽団,THE IDOLM@STER Series,Bandai Namco Game Music",
+            },
+          ],
         },
         {
           thumbnail_url: "https://i.scdn.co/image/ab67616d00001e027de4dbffeda7b3ab5a2e5c77",
           title: "Today Is A Beautiful Day",
           year: 2011,
-          artist: "supercell",
+          artists: [
+            {
+              spotify_id: "aaa",
+              name: "supercell",
+            },
+          ],
         },
         {
           thumbnail_url: "https://i.scdn.co/image/ab67616d00001e027de4dbffeda7b3ab5a2e5c77",
           title: "Today Is A Beautiful Day",
           year: 2011,
-          artist: "supercell",
+          artists: [
+            {
+              spotify_id: "aaa",
+              name: "supercell",
+            },
+          ],
         },
         {
           thumbnail_url: "https://i.scdn.co/image/ab67616d00001e027de4dbffeda7b3ab5a2e5c77",
           title: "Today Is A Beautiful Day",
           year: 2011,
-          artist: "supercell",
+          artists: [
+            {
+              spotify_id: "aaa",
+              name: "supercell",
+            },
+          ],
         },
       ],
     )
