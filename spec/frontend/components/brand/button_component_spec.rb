4b797bf15f1735081a31a8e8c79f811e73f13bad
subroh0508 <subroh.0508@gmail.com>
2025-01-04 02:20:12 +0900

    Refactor button component specs to use separate label attribute

--- spec/frontend/components/brand/button_component_spec.rb
+++ spec/frontend/components/brand/button_component_spec.rb
@@ -5,8 +5,9 @@ require "rails_helper"
 RSpec.describe Brand::ButtonComponent, type: :component do
   shared_examples "button renders" do |args|
     let(:brand) { args[0] }
-    let(:anchor_classes) { args[1] }
-    let(:icon) { args[2] }
+    let(:label) { args[1] }
+    let(:anchor_classes) { args[2] }
+    let(:icon) { args[3] }
 
     let(:href) { "/example" }
     let(:icon_classes) { ["fab", icon, "fa-lg"] }
@@ -17,7 +18,7 @@ RSpec.describe Brand::ButtonComponent, type: :component do
         href: href,
       ))
 
-      expect(page).to have_link(brand, href: href)
+      expect(page).to have_link(label, href: href)
       expect(page).to have_selector("a")
       expect(page.find("i")["class"]).to include(*icon_classes)
     end
@@ -29,7 +30,7 @@ RSpec.describe Brand::ButtonComponent, type: :component do
         active: true,
       ))
 
-      expect(page).to have_link(brand, href: href)
+      expect(page).to have_link(label, href: href)
       expect(page.find("i")["class"]).to include(*icon_classes)
       expect(page.find("a")["class"]).to include(*anchor_classes)
     end
@@ -37,16 +38,19 @@ RSpec.describe Brand::ButtonComponent, type: :component do
 
   it_behaves_like "button renders", [
     Api::YOUTUBE_MUSIC,
+    Brand::YOUTUBE_MUSIC,
     %w[text-white bg-youtubemusic],
     "fa-youtube",
   ]
   it_behaves_like "button renders", [
     Api::SPOTIFY,
+    Brand::SPOTIFY,
     %w[text-black bg-spotify],
     "fa-spotify",
   ]
   it_behaves_like "button renders", [
     Api::APPLE_MUSIC,
+    Brand::APPLE_MUSIC,
     %w[text-white bg-applemusic],
     "fa-apple",
   ]
