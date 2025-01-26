170e04b23c32d28dba97f484cdf3aa85a5b482b9
subroh0508 <subroh.0508@gmail.com>
2025-01-27 01:15:16 +0900

    Add Albums::YoutubeMusic::PlayerComponent

---
+++ app/frontend/sections/albums/youtube_music/player_component.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Albums
+  module YoutubeMusic
+    class PlayerComponent < ViewComponent::Base
+      def initialize(
+        youtube_music_id: nil
+      )
+        @youtube_music_id = youtube_music_id
+      end
+
+      def src
+        "https://www.youtube.com/embed/videoseries?list=#{youtube_music_id}"
+      end
+
+      private
+
+      attr_reader :youtube_music_id
+    end
+  end
+end
