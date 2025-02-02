2f3465e2e8d49561ef2391630cbfcb0a1e14fdcc
subroh0508 <subroh.0508@gmail.com>
2025-02-02 21:47:14 +0900

    Add albums/:id/player

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,26 +2,28 @@
 
 class AlbumsController < ApplicationController
   def index
-    @albums = Album.all.map { |album|
-      album.to_json_hash("ja")
-    }
+    @album = get_album
   end
 
-  def show
-    album = Album.find(params[:id])
-
-    tracks = album.tracks.map { |track|
-      track.to_json_hash("ja")
-    }
-
+  def player
     @streaming_service = streaming_service
-    @album = album.to_json_hash("ja").
-      merge(tracks: tracks)
+    @album = get_album
   end
 
   private
 
   def streaming_service
     params[:streaming_service] || Api::SPOTIFY
+  end
+
+  def get_album
+    album = Album.find(params[:id])
+
+    tracks = album.tracks.map { |track|
+      track.to_json_hash("ja")
+    }
+
+    album.to_json_hash("ja").
+      merge(tracks: tracks)
   end
 end
