5fa8020ec65bb5937041bd18f7287cbbcb4d1e28
subroh0508 <subroh.0508@gmail.com>
2025-02-10 00:58:42 +0900

    Fix test

--- spec/frontend/components/link/button_component_spec.rb
+++ spec/frontend/components/link/button_component_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Link::ButtonComponent, type: :component do
       "dark:text-neutral-400",
       "dark:bg-neutral-800",
       "dark:hover:text-white",
-      "dark:hover:bg-neutral-600",
+      "dark:hover:bg-neutral-700",
     )
   end
 
