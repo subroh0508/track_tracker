3aff9ecfd6b913425ee54d1621628e8ea4438ca2
subroh0508 <subroh.0508@gmail.com>
2025-02-16 14:49:31 +0900

    Add user location storage for Devise sign-in and sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || request.referer || super
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
 end
