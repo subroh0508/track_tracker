9d8612006ac7a83fe8e648022b686a3ba481b5c0
subroh0508 <subroh.0508@gmail.com>
2025-02-16 16:58:16 +0900

    Update users table to use UUIDs for primary key and set default value

--- db/schema.rb
+++ db/schema.rb
@@ -132,7 +132,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_09_095356) do
     t.index ["track_id"], name: "index_translations_tracks_on_track_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
