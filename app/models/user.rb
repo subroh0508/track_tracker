67b456356c6f45cc82b78d9449c824d74291056a
subroh0508 <subroh.0508@gmail.com>
2025-02-16 18:11:00 +0900

    Add validation for user name presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
